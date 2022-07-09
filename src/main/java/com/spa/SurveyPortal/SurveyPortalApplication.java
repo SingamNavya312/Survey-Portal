a138b29be9eee589a913c356dcf18830ace7a588
SingamNavya312 <86612637+SingamNavya312@users.noreply.github.com>
2022-07-09 14:18:57 +0530

    'Project'

---
+++ src/main/java/com/spa/SurveyPortal/SurveyPortalApplication.java
@@ -0,0 +1,13 @@
+package com.spa.SurveyPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spa/SurveyPortal/SurveyPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spa.SurveyPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
